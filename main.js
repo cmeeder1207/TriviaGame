78a924dc4cef0bb5bbe41265241aed75d638a4d3
Chris Meeder <cmeeder1207@gmail.com>
2018-08-09 18:34:16 -0700

    i relized that it only counts things when you hit submit. i need to figure out a way to fix that

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 
 
-var answers = ["js","potato","own","plumbis","hot","data","42","dance","crabCore","flight","Plumbis"]
+var answers = ["js","potato","own","plumbis","hot","data","42","dance","crabCore","hi","Plumbis"];
 
 
 var audioElement = document.createElement("audio");
 audioElement.setAttribute("src", "assets/hi.mp3");
 
-window.onload = function(){
-    $("#start").on("click", function(){
-        startTimer();
-    })
-    // timer--; // TODO: Is there a reason why this timer is still here? Doesn't seem to have been declared anywhere nor is used anywhere else
-};
 
-var timeLeft = 30;
+
+$("#start").on("click", function(){
+    startTimer();
+    audioElement.play()
+});
+
+
+var timeLeft = 126;
 var correct = 0;
 var wrong = 0;
 var unanswered = 0;
 var intervalID;
 
 function startTimer() {
+    
     intervalID = setInterval(count, 1000);
     $("#timeLeft").text(timeLeft);
     $("#buttons").hide()
@@ -28,7 +30,17 @@ function startTimer() {
 
 function count(){
     timeLeft--;
+    
     $("#timeLeft").text(timeLeft);
+    if(timeLeft === 60||timeLeft===15||timelife===5||timelife===3){
+        $("body").css("background-image", "url(https://78.media.tumblr.com/be5282deb7246c88281bf137efd33cd7/tumblr_ni4kiqzYgE1rnq3cto1_500.gif)");
+    }
+    if(timeLeft===30||timelife===10||timelife===4||timelife===2||timelife===1){
+        $("body").css("background-image", "url(https://giant.gfycat.com/HarshDetailedFieldspaniel.gif)");
+    }
+
+    
+
     if(timeLeft === 0){
         clearInterval(intervalID);
         $("#timer").hide()
@@ -37,6 +49,7 @@ function count(){
         $("#correct").text(correct)
         $("#wrong").text(wrong)
         $("#unanswered").text(unanswered)
+        $("body").css("background-image", "url(https://78.media.tumblr.com/be5282deb7246c88281bf137efd33cd7/tumblr_ni4kiqzYgE1rnq3cto1_500.gif)");
     }
 }
 
@@ -45,9 +58,16 @@ function gameOver(){
         $("#timer").hide()
         $("#questions").hide()
         $("#end").show()
-        $("#correct").text(correct)
-        $("#wrong").text(wrong)
-        $("#unanswered").text(unanswered)
+        $("#correct").append('<h1> '+ +correct +'</h1>')
+        $("#wrong").append('<h1> ' + wrong +'</h1>')   
+          if(unanswered=11){
+            unanswered=10;
+        }
+        $("#unanswered").append('<h1> ' + unanswered +'</h1>')
+   
+        console.log(correct);
+        console.log(wrong)
+        console.log(unanswered)
 }
 
 $(document).on('click', '#submit', function(event){
@@ -55,7 +75,7 @@ $(document).on('click', '#submit', function(event){
 
     for (let i = 0; i < answers.length; i++) {
 
-        debugger;
+        
         if($(`input[name=q${i}]:checked`).attr("ans") == answers[i]){
             correct++;
             // unanswered--;
